43131cc29925464f862ed6c7d15a0bfe3b4acd76
Senpaizuri <mjsleebos@gmail.com>
2019-03-22 10:51:57 +0100

    Remove redundant extra step

--- assignment/src/js/script.js
+++ assignment/src/js/script.js
@@ -1,11 +1,48 @@
-import * as scanner from "/js/modules/scanner.js"
-(()=>{
-    'use strict'
-    const
-        app = {
-            init:()=>{
-                scanner
-            }
+const
+    init = ()=>{
+        if (navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function') {
+            Quagga.init({
+                inputStream : {
+                    name : "Live",
+                    type : "LiveStream",
+                    target: finder
+                  },constraints: {
+                    width: 480,
+                    height: 480
+                  },
+                  decoder : {
+                    readers : ["ean_reader"]
+                  },
+                  debug:{
+                      drawScanline:true
+                  }
+                }, function(err) {
+                    if (err) {
+                        console.log(err);
+                        return
+                    }
+                    console.log("Initialization finished. Ready to start");
+                    Quagga.start();
+            })
         }
-    app.init()
+        detect()
+    },
+    detect = ()=>{
+        Quagga.onDetected((data)=>{
+            let
+                code = data.codeResult.code
+            finder.classList.remove("active")
+            console.log("code found", data.codeResult.code)
+            Quagga.stop()
+            window.location.pathname = "album/" + code
+        })
+    }
+
+(()=>{
+    let finder = document.querySelector("#finder")
+
+    document.querySelector("[data-start]").addEventListener("click",()=>{
+        finder.classList.add("active")
+        init()
+    })
 })()
